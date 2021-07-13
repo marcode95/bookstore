b8a6f2b503f3b92696ead164080f0c8c4d30c64d
Marco <marcoerhardt95@gmail.com>
2021-07-13 19:06:29 +0200

    Add BooksForm component

---
+++ src/containers/BooksForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Book from '../components/Book';
+
+const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"]
+
+const BooksForm = () => (
+  <div>
+    <form>
+      <label for="bookTitle">Book title:</label><br />
+      <input type="text" id="bookTitle" /><br />
+      <select id="bookCategory">
+        {categories.map((category) => (<option value={category}>{category}</option>))}
+      </select>
+      <input type="submit" id="bookSubmit" />
+    </form>
+  </div>
+);
