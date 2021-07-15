828386737982fad72d5fc88725f829773c9e70b2
Marco <marcoerhardt95@gmail.com>
2021-07-15 11:16:03 +0200

    Resolve merge conflict

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import '../App.css';
+import BooksForm from '../containers/BooksForm';
+import BooksList from '../containers/BooksList';
 
-function App() {
-  return (
-    <div className="App" />
-  );
-}
+const App = () => (
+  <div>
+    <BooksList />
+    <BooksForm />
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
