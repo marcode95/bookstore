8f969c34e285dab0bb0768f72bb00e3cc65e27d3
Marco <marcoerhardt95@gmail.com>
2021-07-16 12:45:30 +0200

    Add handleChange and handleSubmit methods to booksform.js, connect new probs to redux store, fix bug by removing paranthesis from handleChange call in return function

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createBook } from '../actions/index';
 
 const categories = ['Action', 'Biography', 'Drama', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const BooksForm = () => (
-  <div>
-    <form>
-      <p>Book title:</p>
-      <br />
-      <input type="text" id="bookTitle" />
-      <br />
-      <select id="bookCategory">
-        {categories.map((cat) => (<option value={cat} key={cat}>{cat}</option>))}
-      </select>
-      <input type="submit" id="bookSubmit" />
-    </form>
-  </div>
-);
+const BooksForm = ({ createBook }) => {
+  const [state, setState] = useState(
+    {
+      id: '',
+      title: '',
+      category: '',
+    },
+  );
 
-export default BooksForm;
+  const handleChange = (e) => {
+    const newState = { ...state };
+    newState[e.target.name] = e.target.value;
+    setState(newState);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createBook({ ...state, id: Math.floor(Math.random() * 100) });
+    setState({
+      title: '',
+      category: '',
+    });
+  };
+
+  return (
+    <div>
+      <form method="POST" onSubmit={handleSubmit}>
+        <p>Book title:</p>
+        <br />
+        <input type="text" id="bookTitle" name="title" value={state.title} onClick={handleChange} />
+        <br />
+        <select id="bookCategory" onClick={handleChange}>
+          {categories.map((cat) => (<option name="category" value={state.category} key={cat}>{cat}</option>))}
+        </select>
+        <input type="submit" id="bookSubmit" />
+      </form>
+    </div>
+  );
+};
+
+export default connect(null, { createBook })(BooksForm);
+
+BooksForm.propTypes = {
+  createBook: PropTypes.func.isRequired,
+};
